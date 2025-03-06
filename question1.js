bfed0d7bc8e997411acb5897ccac9a1fde26ceb0
Emily Smith <etsmith9@gmail.com>
2025-03-06 11:10:57 -0500

    Add files via upload

---
+++ question1.js
@@ -0,0 +1,11 @@
+// Task 1 & 2, Declare variables and values
+let studentName = "Steve";
+let studentAge = 17;
+let studentGrade = 11;
+let isPresent = true;
+
+// Task 3, display student information using console.log()
+console.log(`Information for student: ${studentName}`);
+console.log(`- age: ${studentAge}`);
+console.log(`- grade: ${studentGrade}`);
+console.log(`- Is present? ${isPresent? "Yes" : "No"}`);
---
+++ question2.js
@@ -0,0 +1,29 @@
+
+// Task 1 & 2, Declare variables and values
+let a = 10;
+let b = 5;
+
+// Task 3, Perform arithmetic operations 
+console.log("Sum:", a + b);
+console.log("Difference:", a - b);
+console.log("Product:", a * b);
+console.log("Quotient:", a / b);
+
+// Task 4, Explore assignment operators and update the values of variables
+
+console.log("Updated num1:", a += b);
+console.log("Updated num2:", a *= b);
+
+let num1 = 15;
+let num2 = 75;
+
+// Task 5: Use comparison operators
+console.log("Is num1 equal to num2?", num1 == num2);
+console.log("Is num1 greater than num2?", num1 > num2);
+console.log("Is num1 not equal to num2?", num1 != num2);
+
+// Task 6: Apply logical operators
+let isPositive = 10;
+let isEven = 12;
+console.log("Are both numbers positive?", isPositive > 0 && isEven > 0 );
+console.log("Is at least one number even?", isEven % 2 === 0 || isPositive % 2 === 0);
